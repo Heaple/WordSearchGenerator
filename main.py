c39f246bb71a703cdc2f03f5fb860cd486807544
Heaple <Heaple1021@gmail.com>
2021-12-14 12:54:24 +0900

    add dictionary to set direction

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import *
 
 word_search = []
 total_word_list = []
+word_dict = {"c": [], "w": [], "h": []}
 
 word_count = int(input("How many word you will include in word search? "))
 width = int(input("How long is the width? "))
@@ -13,6 +14,16 @@ for i in range(word_count):
     for j in range(len(word)):
         word_letter.append(word[j])
     total_word_list.append(word_letter)
+    if len(word_letter) > height:
+        if len(word_letter) > width:
+            word_dict["c"].append(word_letter)
+        else:
+            random_dir = ["c", "w"]
+            word_dict[choice(random_dir)].append(word_letter)
+    else:
+        random_dir = ["w", "h"]
+        word_dict[choice(random_dir)].append(word_letter)
+    print(word_dict["c"], word_dict["h"], word_dict["w"])
 
 for i in range(height):
     have_to_append = []
