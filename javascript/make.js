ea7c0fc49f01bb0d80d01f9dad9a7b8496913b22
Heaple <Heaple1021@gmail.com>
2021-12-16 18:21:29 +0900

    added css to select

---
+++ javascript/make.js
@@ -0,0 +1,6 @@
+import * as settings from "./select.js";
+
+for (i=0; i <= width; i ++) {
+    document.createElement('input') * settings.width;
+    console.log(settings.height);
+}
--- javascript/select.js
+++ javascript/select.js
@@ -1,29 +1,49 @@
-let width_textbox = document.getElementById("width");
-let height_textbox = document.getElementById("height");
+let width_textbox = document.querySelector("#width");
+let height_textbox = document.querySelector("#height");
 let width = null;
 let height = null;
 
-const makebutton = document.querySelector("#make-button");
+const makebutton = document.getElementById("make-button");
+const activeclass = "active";
 
 function handleClickMakeBtn() {
+
     width = parseInt(width_textbox.value);
     height = parseInt(height_textbox.value);
     if (width > 30 || height > 30) {
         alert("Width and height cannot longer than 30!");
-    } else if (width < 5 || height < 5) {
+        width_textbox.value = "";
+        height_textbox.value = "";
+
+        } else if (width < 5 || height < 5) {
         alert("Width and height cannot shorter than 5!");
         width_textbox.value = "";
         height_textbox.value = "";
+
     } else if (isNaN(width) || isNaN(height)) {
         alert("Width and height must be number!");
         width_textbox.value = "";
         height_textbox.value = "";
+
     } else if (width == null || height == null) {
         alert("You have to enter width and height!");
     } 
+
     else {
         window.location.href = "/html/make.html";
     }
 }
 
+function handleMakeBtnMouseEnter() {
+    makebutton.classList.add(activeclass);
+}
+
+function handleMakeBtnMouseLeave() {
+    makebutton.classList.remove(activeclass);
+}
+
+export { width, height };
+
+makebutton.addEventListener("mouseenter", handleMakeBtnMouseEnter);
+makebutton.addEventListener("mouseleave", handleMakeBtnMouseLeave);
 makebutton.addEventListener("click", handleClickMakeBtn);
