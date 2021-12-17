f6b70ef1c3a676036bcbf913096751edfa6b4bc7
Heaple <Heaple1021@gmail.com>
2021-12-17 16:02:38 +0900

    add wordCount

--- javascript/make.js
+++ javascript/make.js
@@ -1,1 +1,25 @@
 import * as settings from "./select.js";
+
+const letter = document.querySelector(".letter");
+let added = false
+let i;
+let j;
+
+function createSheet() {
+    if (added == false) {
+        for (i = 0; i <= settings.height; i++) {
+            let nowLine = document.createElement("div");
+            nowLine.id = toString(i)+"line"
+            letter.appendChild(nowLine);
+            for (j = 0; j <= settings.width; j++) {
+                let nowColumn = document.createElement("input");
+                nowColumn.type = "text";
+                nowColumn.id = toString(j)+"column";
+                nowColumn.style = "width:5px;height:5px;font-size:3px;";
+                nowLine.appendChild(nowColumn);
+            }
+        }
+    }
+}
+
+window.onload = createSheet();
--- javascript/select.js
+++ javascript/select.js
@@ -1,7 +1,10 @@
 let width_textbox = document.querySelector("#width");
 let height_textbox = document.querySelector("#height");
-let width = null;
-let height = null;
+let wordCount_textbox = document.querySelector("#word-count")
+
+let width;
+let height;
+let wordCount;
 
 const makebutton = document.getElementById("make-button");
 const activeclass = "active";
@@ -10,25 +13,42 @@ function handleClickMakeBtn() {
 
     width = parseInt(width_textbox.value);
     height = parseInt(height_textbox.value);
+    wordCount = parseInt(wordCount_textbox.value)
+
     if (width > 30 || height > 30) {
-        alert("Width and height cannot longer than 30!");
+        alert("Width and height cannot longer than 30.");
         width_textbox.value = "";
         height_textbox.value = "";
 
-        } else if (width < 5 || height < 5) {
-        alert("Width and height cannot shorter than 5!");
+    } else if (width < 5 || height < 5) {
+        alert("Width and height cannot shorter than 5.");
         width_textbox.value = "";
         height_textbox.value = "";
 
     } else if (isNaN(width) || isNaN(height)) {
-        alert("Width and height must be number!");
+        alert("Width and height must be number.");
         width_textbox.value = "";
         height_textbox.value = "";
 
-    } else if (width == null || height == null) {
-        alert("You have to enter width and height!");
-    } 
+    } else if (wordCount < 1) {
+        wordCount_textbox.value = "";
+        alert("Number of words must more than 0.");
 
+    } else if (width == null || height == null) {
+        wordCount_textbox.value = "";
+        alert("You have to enter width and height.");
+
+    }  else if (wordCount > 30) {
+        wordCount_textbox.value = "";
+        alert("Number of words must less than 31.");
+
+    } else if (wordCount == null) {
+        alert("You have to enter number of words.");
+
+    } else if (isNaN(wordCount)) {
+        wordCount_textbox.value = "";
+        alert("Number of words must be number.")
+    }
     else {
         window.location.href = "/html/make.html";
     }
@@ -42,7 +62,7 @@ function handleMakeBtnMouseLeave() {
     makebutton.classList.remove(activeclass);
 }
 
-export { width, height };
+export { width, height, wordCount };
 
 if (makebutton) {
     makebutton.addEventListener("mouseenter", handleMakeBtnMouseEnter);
